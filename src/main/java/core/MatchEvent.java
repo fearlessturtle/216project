cabdcaef9a4b704b20784c0fcd891d24c4055ec1
jonathan <jonathanmertcrowell@gmail.com>
2026-03-20 09:18:22 +0300

    Add MatchObserver interface and MatchEvent class

---
+++ src/main/java/core/MatchEvent.java
@@ -0,0 +1,67 @@
+package core;
+
+public class MatchEvent {
+
+    public enum EventType {
+        GOAL,
+        YELLOW_CARD,
+        RED_CARD,
+        SUBSTITUTION,
+        INJURY,
+        PERIOD_END,
+        MATCH_END
+    }
+
+    private EventType type;
+    private int minute;
+    private String player;
+    private String team;
+    private String desc;
+
+    public MatchEvent(EventType type, int minute, String player, String team, String desc) {
+        this.type = type;
+        this.minute = minute;
+        this.desc = desc;
+
+        if (type == EventType.PERIOD_END || type == EventType.MATCH_END) {
+            this.player = null;
+            this.team = null;
+        } else {
+            this.player = player;
+            this.team = team;
+        }
+    }
+
+    public static MatchEvent periodEnd(int minute) {
+        return new MatchEvent(EventType.PERIOD_END, minute, null, null, null);
+    }
+
+    public static MatchEvent matchEnd(int minute) {
+        return new MatchEvent(EventType.MATCH_END, minute, null, null, null);
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getDescription() {
+        return desc;
+    }
+
+    @Override
+    public String toString() {
+        return type + " at minute " + minute + " | player: " + player + " team: " + team;
+    }
+}
---
+++ src/main/java/core/MatchObserver.java
@@ -0,0 +1,5 @@
+package core;
+
+public interface MatchObserver {
+    void onEvent(MatchEvent event);
+}
