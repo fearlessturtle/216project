76244decf55f7935c3e88ca002cdfe1d39004703
jonathan <jonathanmertcrowell@gmail.com>
2026-03-23 00:23:59 +0300

    [J02] Write AbstractMatch

---
+++ src/main/java/com/sportsmanager/core/AbstractMatch.java
@@ -0,0 +1,78 @@
+package com.sportsmanager.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractMatch implements Match {
+
+    protected Team homeTeam;
+    protected Team awayTeam;
+    protected int homeScore;
+    protected int awayScore;
+    protected boolean completed;
+    protected List<MatchObserver> observers;
+    protected List<MatchEvent> events;
+
+    public AbstractMatch(Team homeTeam, Team awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+        this.completed = false;
+        this.observers = new ArrayList<>();
+        this.events = new ArrayList<>();
+    }
+
+    @Override
+    public final void play() {
+        for (int i = 1; i <= getPeriodCount(); i++) {
+            simulatePeriod(i);
+        }
+        completed = true;
+        notifyObservers(MatchEvent.matchEnd(getPeriodCount() * 45));
+    }
+
+    protected abstract void simulatePeriod(int periodNumber);
+
+    protected abstract int getPeriodCount();
+
+    protected void notifyObservers(MatchEvent event) {
+        events.add(event);
+        for (MatchObserver observer : observers) {
+            observer.onEvent(event);
+        }
+    }
+
+    public void addObserver(MatchObserver observer) {
+        observers.add(observer);
+    }
+
+    public List<MatchEvent> getEvents() {
+        return new ArrayList<>(events);
+    }
+
+    @Override
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    @Override
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    @Override
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    @Override
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return completed;
+    }
+}
