fecf94f21646387781be6edc6daffcc078001ea4
Milena Ünal <114659482+fearlessturtle@users.noreply.github.com>
2026-04-11 00:08:35 +0300

    .
    .

--- src/main/java/com/sportsmanager/core/Match.java
+++ src/main/java/com/sportsmanager/core/Match.java
@@ -11,7 +11,11 @@ public interface Match {
     int getAwayScore();
     boolean isCompleted();
 
+<<<<<<< Updated upstream
     int[] getScore();
+=======
+    int getScore();
+>>>>>>> Stashed changes
     List<MatchEvent> getMatchEvents();
     void applyTacticChange(Team team, Tactic newTactic);
     void substitutePlayer(Team team, Player playerOut, Player playerIn);
--- src/main/java/com/sportsmanager/core/Team.java
+++ src/main/java/com/sportsmanager/core/Team.java
@@ -11,7 +11,6 @@ public interface Team {
     List<Player> selectLineup();
     void trainWeek();
 
-
     List<Coach> getCoaches();
     Tactic getTactic();
     void setTactic(Tactic tactic);
